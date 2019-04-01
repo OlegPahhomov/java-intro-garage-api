60058a5633b43bff8f20382f1a4142dd78f98f1e
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2019-04-01 11:11:41 +0300

    Add code written in class

--- src/main/java/ee/itcollage/garageapi/controller/CustomerController.java
+++ src/main/java/ee/itcollage/garageapi/controller/CustomerController.java
@@ -2,13 +2,22 @@ package ee.itcollage.garageapi.controller;
 
 
 import ee.itcollage.garageapi.model.Customer;
+import ee.itcollage.garageapi.repository.CustomerRepository;
 import ee.itcollage.garageapi.server.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/customer")
@@ -16,10 +25,44 @@ public class CustomerController {
 
     @Autowired
     private CustomerService customerService;
+    @Autowired
+    private CustomerRepository customerRepository;
 
     @GetMapping
     public List<Customer> findAll() {
         return customerService.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Customer findById(@PathVariable Long id) {
+        Optional<Customer> byId = customerRepository.findById(id);
+        return byId.orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.BAD_REQUEST, "id doesnt exist"));
+    }
+
+    @PostMapping
+    public Customer save(@RequestBody Customer customer) {
+        customer.setId(null);
+        return customerRepository.save(customer);
+    }
+
+    @PutMapping("{id}")
+    public Customer update(@PathVariable Long id, @RequestBody Customer customer) {
+        Customer byId = findById(id);
+        byId.setFirstName(customer.getFirstName());
+        byId.setLastName(customer.getLastName());
+        return customerRepository.save(byId);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable Long id) {
+        Customer byId = findById(id);
+        customerRepository.delete(byId);
+    }
+
+    @DeleteMapping("/v2/{id}")
+    public void delete2(@PathVariable Long id) {
+        customerRepository.deleteById(id);
     }
 
     //todo
